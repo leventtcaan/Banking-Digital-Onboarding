89398f84f7813b1b5f348af7935f2fe7270a6ca6
Levent Can Ceylan <ceylanleventcan@gmail.com>
2026-04-22 14:42:07 +0300

    feat: create CustomerApplication domain model

---
+++ Models/CustomerApplication.cs
@@ -0,0 +1,11 @@
+namespace Banking_Digital_Onboarding
+{
+      public class CustomerApplication
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string NationId { get; set; }
+        public decimal MonthlyIncome { get; set; }
+        public string ApplicationType { get; set; }
+    }
+}
